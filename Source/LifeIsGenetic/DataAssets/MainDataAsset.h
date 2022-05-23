4ca06eeacca1cf970db8a42deaf44fbf8b4ddeb6
sphilipps <samuel.philipps@gmail.com>
2022-05-23 15:30:44 +0200

    Add Managers and Individual in MainDataAsset

--- Source/LifeIsGenetic/DataAssets/MainDataAsset.h
+++ Source/LifeIsGenetic/DataAssets/MainDataAsset.h
@@ -24,14 +24,118 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Global")
 		FVector WorldPosition;
 
+	// GenerationManager
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GenerationManager")
 		int IndividualsMaxCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GenerationManager")
+		float DamageSpeedNumerator;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GenerationManager")
+		float NavmeshRadiusToSpawn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GenerationManager")
+		float LegsLengthMutationRatio;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GenerationManager")
+		float ThickSkinMutationRatio;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GenerationManager")
+		float SharpedTeethMutationRatio;
+
+	// Individual
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual")
+		float DisplacementSpeedFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual")
+		float SpawnCorpseRatio;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual")
+		float BulletDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual")
+		float FruitHeal;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float DefensePlusFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float DefenseMinusFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		int DefenseMinusClampMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		int DefenseMinusClampMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float AttackPlusFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float AttackMinusFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		int AttackMinusClampMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		int AttackMinusClampMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float SpeedPlusFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float SpeedPlusClampMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float SpeedPlusClampMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float SpeedMinusClampMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float SpeedMinusClampMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float PoopPlusFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float PoopPlusClampMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float PoopPlusClampMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float PoopMinusClampMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Individual > Genes")
+		float PoopMinusClampMax;
+
+	// WeatherManager
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeatherManager")
 		float ChanceOfEvent;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeatherManager")
 		float ChanceOfRain;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeatherManager")
+		float NotificationDuration;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeatherManager")
+		float TimeBetweenEventsMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeatherManager")
+		float TimeBetweenEventsMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeatherManager")
+		float TimeBetweenRainsMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "WeatherManager")
+		float TimeBetweenRainsMax;
+
+	// Poop
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Poop")
 		float AutoGrowTime;
